60bfbbb7f52d4bf98351eb034c9806b9bb0d63f0
Leandro Lucarella <llucax@gmail.com>
2003-07-12 21:39:44 +0000

    Removed \n at the end of the file and added a VIM option to keep it as is.

--- xmi2code.tpl.php
+++ xmi2code.tpl.php
@@ -1,5 +1,5 @@
 <?php
-// vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker binary:
+// vim: set expandtab tabstop=4 shiftwidth=4 binary:
 // +--------------------------------------------------------------------+
 // |                       BIFE - Buil It FastEr                        |
 // +--------------------------------------------------------------------+
@@ -19,48 +19,9 @@
 // | along with Hooks; if not, write to the Free Software Foundation,   |
 // | Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA      |
 // +--------------------------------------------------------------------+
-// | Created: Wed May 17 18:16:54 ART 2003                              |
+// | Created: @@date                                  |
 // | Authors: Leandro Lucarella <luca@lugmen.org.ar>                    |
 // +--------------------------------------------------------------------+
 //
 // $Id$
 //
-
-// Inicialization {{{
-ini_set('include_path', '../src:../../hit/src:../../bife/src:../../base/src:'.
-    ini_get('include_path'));
-umask('002');
-require_once 'HTML/Template/HIT.php';
-require_once 'BIFE/Parser.php';
-require_once 'BIFE/Translate.php';
-// }}}
-
-// Selects the file to view {{{
-$file = (@$_SERVER['PATH_INFO'] and ($_SERVER['PATH_INFO'] != '/'))
-    ? ".{$_SERVER['PATH_INFO']}"
-    : 'index.xbf';
-// }}}
-
-// Looks if we want to show the source {{{
-if (@$_REQUEST['S']) {
-    if (@$_REQUEST['B']) {
-        // We want to see the BIFE file source.
-        echo '<PRE>';
-        echo htmlentities(join('', file($file)));
-    } else {
-        // We want to see the php file source.
-        highlight_file($_SERVER['SCRIPT_FILENAME']);
-    }
-    exit;
-}
-// }}}
-
-// If we are not looking at the source, we use BIFE to show the page {{{
-$template =& new HTML_Template_HIT('templates');
-$parser   =& new BIFE_Parser('BIFE_Translate');
-$page     =& $parser->parseFile($file);
-$parser->__destruct();
-echo $page->render($template);
-// }}}
-
-?>
